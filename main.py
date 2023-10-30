89b751ee1d0131194cbdcf7f5dbbd12d3385d86b
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2023-10-31 01:26:18 +0900

    :sparkles: llamaindex回答処理実装

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from llama_index import StorageContext, load_index_from_storage
 
 app = FastAPI()
 
@@ -6,6 +7,11 @@ app = FastAPI()
 def index():
     return {"Hello": "World"}
 
-@app.get("/query/{text}")
-async def index2(text: str):
-    return {"Hello": f"World I am {text}"}
+@app.get("/query/{query}")
+async def answer_with_llamaindex(text: str):
+    # インデックスの読み込み
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine()
+    answer = query_engine.query("質問：{query}")
+    return {"Answer": f"{answer.response}"}
